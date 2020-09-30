adc5d0ce092079aa8a5e96bc2d6ff8450eb03787
ArronSchick <arron.schick@gmail.com>
2020-09-30 18:23:24 +1000

    instance variable for user name input added

--- src/new_game.rb
+++ src/new_game.rb
@@ -4,9 +4,12 @@ require 'colorize'
 require 'tty-prompt'
 
 class New_game
+    def initialize
+        @name
+    end
     def user_name
         prompt = TTY::Prompt.new
-        name = prompt.ask("What is your name?")
+        @name = prompt.ask("What is your name?")
         Clean_up.clear_screen
     end
 end
